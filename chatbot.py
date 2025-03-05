aa1d04e426a556ee5f25745eac4aad5be0f83ed3
Elizabeth2304 <barvaelizabeth@gmail.com>
2025-03-04 20:38:47 -0500

    commit

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 
-respuesta = {
+respuestas = {
     "hola como estas": "muy bien en que puedo ayudarte",
-    "donde se encuentran uicados": "en la cra 29#50-20",
+    "donde se encuentran ubicados": "en la cra 29#50-20",
     "que curso tienen disponibles": "tenemos bootcamps de prograacion y analisis de datos",
     "cuando inician los cursos": "cada primera semana del mes",
     "cuanto dura el curso": "un mes y medio",
@@ -9,12 +9,12 @@ respuesta = {
 }
 
 def generar_respuesta(pregunta):
-    if pregunta in respuesta:
-        return respuesta[pregunta]
+    if pregunta in respuestas:
+        return respuestas[pregunta]
     else:
         return "no entiendo tu pregunta"
-
-print("hola en que puedo ayudarte\n")  
+    
+print("hola soy un chatbot en que puedo ayudarte\n")
 while True:
     prompt = input("ingresa tu pregunta\n")
     if prompt == "salir":
